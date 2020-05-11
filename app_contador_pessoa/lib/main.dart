38a378f76c7c6ab8cee97cfb927307797b5d3239
Luis <cespop@gmail.com>
2020-05-11 00:16:51 -0300

    Stateless e Stateful

--- app_contador_pessoa/lib/main.dart
+++ app_contador_pessoa/lib/main.dart
@@ -2,71 +2,100 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      title: 'Contador de Pessoas',
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            'images/original.jpg',
-            fit: BoxFit.cover,
-            height: 1000.00,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Pessoas: 0',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
+    title: 'Contador de Pessoas',
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _people = 0;
+  String _infoText = "Pode Entrar!";
+
+  void _chagePeople(int value) {
+    setState(() {
+      _people += value;
+      if (_people < 0) {
+        _infoText = "Valor Negativo";
+      } else if (_people <= 10) {
+        _infoText = "Pode Entrar...";
+      } else {
+        _infoText = "Lotado!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          'images/original.jpg',
+          fit: BoxFit.cover,
+          height: 1000.00,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pessoas: $_people',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        '+1',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40.0,
-                          fontStyle: FontStyle.italic,
-                        ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      '+1',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40.0,
+                        fontStyle: FontStyle.italic,
                       ),
-                      onPressed: () {
-                        print('Clicou +1.');
-                      },
                     ),
+                    onPressed: () {
+                      _chagePeople(1);
+                    },
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        '-1',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40.0,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                      onPressed: () {
-                        print('Clicou -1.');
-                      },
-                    ),
-                  )
-                ],
-              ),
-              Text(
-                'Pode Entrar!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0,
                 ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      '-1',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40.0,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    onPressed: () {
+                      _chagePeople(-1);
+                    },
+                  ),
+                )
+              ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0,
               ),
-            ],
-          ),
-        ],
-      )));
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
