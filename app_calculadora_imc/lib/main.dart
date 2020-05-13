3628495a94007f8aee873e33a5f14f10a5eff4a6
Luis <cespop@gmail.com>
2020-05-13 00:44:28 -0300

    Inicio: App Calculo do IMC

---
+++ app_calculadora_imc/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora de IMC'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              print('IconButton ...');
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
