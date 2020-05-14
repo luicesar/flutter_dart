3708017b2f808437af2c414c70fbadcf1dde2b0d
Luis <cespop@gmail.com>
2020-05-13 23:00:27 -0300

    Controller e Calculos

--- app_calculadora_imc/lib/main.dart
+++ app_calculadora_imc/lib/main.dart
@@ -14,6 +14,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController pesoController = new TextEditingController();
+  TextEditingController alturaController = new TextEditingController();
+
+  String _infoText = "Informe seus dados";
+
+  void _resetFields() {
+    pesoController.text = '';
+    alturaController.text = '';
+    _infoText = '';
+  }
+
+  void _calculate() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text) / 100;
+      double imc = peso / (altura * altura);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,9 +56,7 @@ class _HomePageState extends State<HomePage> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {
-              print('IconButton ...');
-            },
+            onPressed: _resetFields,
           )
         ],
       ),
@@ -44,7 +74,7 @@ class _HomePageState extends State<HomePage> {
             TextField(
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                labelText: 'Pessoa (kg)',
+                labelText: 'Peso (kg)',
                 labelStyle: TextStyle(color: Colors.green),
               ),
               textAlign: TextAlign.center,
@@ -52,6 +82,7 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.green,
                 fontSize: 20.00,
               ),
+              controller: pesoController,
             ),
             TextField(
               keyboardType: TextInputType.number,
@@ -64,6 +95,7 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.green,
                 fontSize: 20.00,
               ),
+              controller: alturaController,
             ),
             Padding(
               padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
@@ -75,14 +107,12 @@ class _HomePageState extends State<HomePage> {
                     style: TextStyle(color: Colors.white, fontSize: 20.0),
                   ),
                   color: Colors.green,
-                  onPressed: () {
-                    print('button');
-                  },
+                  onPressed: _calculate,
                 ),
               ),
             ),
             Text(
-              'info',
+              _infoText,
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Colors.green,
